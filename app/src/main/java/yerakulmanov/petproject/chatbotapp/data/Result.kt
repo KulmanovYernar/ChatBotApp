ccdb27ef114fbf96e0317cc2dcbf58ca30cb041a
yerakulmanov <200103127@stu.sdu.edu.kz>
2024-11-11 12:18:49 +0500

    Added Result to handle back response events

---
+++ app/src/main/java/yerakulmanov/petproject/chatbotapp/data/Result.kt
@@ -0,0 +1,6 @@
+package yerakulmanov.petproject.chatbotapp.data
+
+sealed class Result<out T> {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+}
