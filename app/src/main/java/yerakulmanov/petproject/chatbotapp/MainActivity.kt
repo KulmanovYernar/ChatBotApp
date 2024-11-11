e8ffe096917efe8a946d1a1b73db90dbfe3c28fb
yerakulmanov <200103127@stu.sdu.edu.kz>
2024-11-11 12:31:40 +0500

    Added NavGraph to MainActivity

--- app/src/main/java/yerakulmanov/petproject/chatbotapp/MainActivity.kt
+++ app/src/main/java/yerakulmanov/petproject/chatbotapp/MainActivity.kt
@@ -5,12 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import yerakulmanov.petproject.chatbotapp.navigation.NavigationGraph
 import yerakulmanov.petproject.chatbotapp.ui.theme.ChatBotAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +18,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+            val authViewModel: AuthViewModel = viewModel()
             ChatBotAppTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavigationGraph(navController = navController, authViewModel = authViewModel)
+                }
             }
         }
     }
--- app/src/main/java/yerakulmanov/petproject/chatbotapp/navigation/NavigationGraph.kt
+++ app/src/main/java/yerakulmanov/petproject/chatbotapp/navigation/NavigationGraph.kt
@@ -1,16 +1,17 @@
-package yerakulmanov.petproject.chatbotapp
+package yerakulmanov.petproject.chatbotapp.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import yerakulmanov.petproject.chatbotapp.navigation.Screen
+import yerakulmanov.petproject.chatbotapp.AuthViewModel
 import yerakulmanov.petproject.chatbotapp.screens.LoginScreen
 import yerakulmanov.petproject.chatbotapp.screens.SignUpScreen
 
 @Composable
 fun NavigationGraph(
-    navController: NavHostController
+    navController: NavHostController,
+    authViewModel: AuthViewModel
 ) {
     NavHost(
         navController = navController,
@@ -18,11 +19,13 @@ fun NavigationGraph(
     ) {
         composable(Screen.SignupScreen.route) {
             SignUpScreen(
+                authViewModel = authViewModel,
                 onNavigateToLogin = { navController.navigate(Screen.LoginScreen.route) }
             )
         }
         composable(Screen.LoginScreen.route) {
             LoginScreen(
+                authViewModel = authViewModel,
                 onNavigateToSignUp = { navController.navigate(Screen.SignupScreen.route) }
             )
         }
